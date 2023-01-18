b88b7fb2d51c0f6514e66d983a90100b90711647
Lailson Sobral <lailsonlm@hotmail.com>
2023-01-17 23:34:46 -0300

    create routes backend and create frontend main layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,14 @@
-function App() {
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
+
+export function App() {
   return (
-    <div className="bg-zinc-600">
-      <h1 className="text-white">Habits</h1>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+
+        <SummaryTable />
+      </div>
     </div>
   )
 }
-
-export default App
